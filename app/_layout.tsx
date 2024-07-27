290fa246ac7bbd44cd8428478fee527ced70e2fa
Diogo Caxiado <caxiadodev@gmail.com>
2024-07-27 13:10:41 -0300

    feat: add transition animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
 
-
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{animation: 'slide_from_right', animationTypeForReplace: 'pop'}}
+    >
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="register/index" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" options={{ headerShown: false }} />
